233c703817eb407424c55cfdb13a6c963d1be899
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-03 09:47:53 +0100

    added

--- Swift-transition/Swift-transition/ViewController.swift
+++ Swift-transition/Swift-transition/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    fileprivate var label = UILabel()
+    
+    fileprivate var button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
